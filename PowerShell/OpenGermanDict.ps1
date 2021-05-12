1cd0207abb91c67d68e9299768b6de46d02eb003
Thach Nguyen <cyborg20125@gmail.com>
2021-05-12 10:09:50 +0700

    Commit 12052021 - Update German Script

---
+++ PowerShell/OpenGermanDict.ps1
@@ -0,0 +1,22 @@
+﻿# Uncomment lines are needed.
+
+# Open URL in default browser Using Powershell
+# Start-Process "website url"
+
+# Open URL in Microsoft Edge Using Powershell
+# [System.Diagnostics.Process]::Start("msedge", "website url")
+
+# Open URL in Google Chrome Using Powershell
+# [System.Diagnostics.Process]::Start("chrome", "https://translate.google.com/?hl=vi&sl=de&tl=en&op=translate")
+
+# Open URL in Firefox Using Powershell
+# [System.Diagnostics.Process]::Start("firefox", "website url")
+
+# Open URL in Internet Explorer Using Powershell
+# [System.Diagnostics.Process]::Start("iexplore", "website url")
+
+# --
+[System.Diagnostics.Process]::Start("chrome", "https://de.langenscheidt.com/")
+[System.Diagnostics.Process]::Start("chrome", "https://www.collinsdictionary.com/dictionary/english-german")
+[System.Diagnostics.Process]::Start("chrome", "https://www.leo.org/englisch-deutsch/")
+[System.Diagnostics.Process]::Start("chrome", "https://translate.google.com/?hl=vi&sl=de&tl=en&text=%0A&op=translate")
---
+++ PowerShell/OpenJapaneseDict.ps1
@@ -0,0 +1,21 @@
+﻿# Uncomment lines are needed.
+
+# Open URL in default browser Using Powershell
+# Start-Process "website url"
+
+# Open URL in Microsoft Edge Using Powershell
+# [System.Diagnostics.Process]::Start("msedge", "website url")
+
+# Open URL in Google Chrome Using Powershell
+# [System.Diagnostics.Process]::Start("chrome", "https://translate.google.com/?hl=vi&sl=de&tl=en&op=translate")
+
+# Open URL in Firefox Using Powershell
+# [System.Diagnostics.Process]::Start("firefox", "website url")
+
+# Open URL in Internet Explorer Using Powershell
+# [System.Diagnostics.Process]::Start("iexplore", "website url")
+
+# --
+[System.Diagnostics.Process]::Start("chrome", "https://jisho.org/")
+[System.Diagnostics.Process]::Start("chrome", "https://jdict.net/")
+[System.Diagnostics.Process]::Start("chrome", "https://translate.google.com/?hl=vi&sl=ja&tl=en&text=%0A&op=translate")
